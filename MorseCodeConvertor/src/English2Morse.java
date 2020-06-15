70248c6c5cec1b52ad3c1d82c8e423d003d36889
Varun Gadde <varungadde99@gmail.com>
2020-06-15 09:28:17 +0530

    Centered Window

--- MorseCodeConvertor/src/English2Morse.java
+++ MorseCodeConvertor/src/English2Morse.java
@@ -28,6 +28,28 @@ class E2M extends JFrame{
 		setLocationRelativeTo(null);
 		setSize(400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		center(this);
 	}
+	
+    public static void center(JFrame frame) {
+    	 
+        // get the size of the screen, on systems with multiple displays,
+        // the primary display is used
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+ 
+        // calculate the new location of the window
+        int w = frame.getSize().width;
+        int h = frame.getSize().height;
+ 
+        int x = (dim.width - w) / 2;
+        int y = (dim.height - h) / 2;
+ 
+        // moves this component to a new location, the top-left corner of
+        // the new location is specified by the x and y
+        // parameters in the coordinate space of this component's parent
+        frame.setLocation(x, y);
+ 
+    }
 	
 }
